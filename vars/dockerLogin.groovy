486c0ad09df4e6fed548f0a6936f20a6c455a31a
lokesh.cloudy <lokeshroyal129@gmail.com>
2021-08-28 18:33:35 +0530

    changing mistakes

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def dockerLogin() {
+def call() {
     echo 'Testing the Image'
     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "echo ${PASS} | docker login -u ${USER} --password-stdin"
