56c582eab074bbdd9d548917b22e26972d87594f
lokesh <76929298+lokeshcloudy@users.noreply.github.com>
2021-08-28 23:11:43 +0530

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -2,5 +2,5 @@
 
 def call(String imageName) {
     echo 'Pushing The Docker Image'
-        sh "docker push ${imageName}/${BRANCH_NAME}:${BUILD_ID}"
+        sh "docker push ${imageName}${BRANCH_NAME}"
 }
